91e6313591c9f5902d3b8340585272ac73df24b7
bhujang21 <indrajeet.2122@gmail.com>
2023-03-05 06:22:00 +0530

    added new class of Linklist with extra methods by my_ll()

--- LinkList_updated.py
+++ LinkList_updated.py
@@ -11,10 +11,10 @@ class LL:
         temp="Head->"
         if self.head != None:
             traval=self.head
-            while traval.next != None:
+            while traval != None:
                 temp+=f"{traval.data}->"
                 traval=traval.next
-            temp+=f"{traval.data}->None"
+            temp+=f"None"
             return temp
         temp+="None"
         return temp
@@ -155,14 +155,13 @@ class LL:
         raise ValueError("LL.pop() Linklist should not be empty")
             
 
-l=LL()
-#
-l.insert_start(20)
+# l=LL()
+# #
 # l.insert_start(10)
+# l.insert_start(20)
 # l.insert_start(30)
-
-# l.insert_start(50)
-# l.insert_start(10)
+# l.insert_start(40)
+# # l.insert_start(10)
 
 
 # print(len(l))
@@ -174,8 +173,293 @@ l.insert_start(20)
 
 # l.remove_start()
 
-print(l)
+# print(l)
 
-l.remove_by_value(40)
+# l.remove_by_value(40)
 
-print(l)
+# print(l)
+
+class my_ll:
+    """this is updated 
+    link list class"""
+
+    def __init__(self):
+        self.head=None
+        self.n=0
+    
+    def __str__(self):
+        """print your list list when hit print() on object"""
+        if self.head:
+            result=""
+            travel=self.head
+            while travel:
+                result+=f"{travel.data}->"
+                travel=travel.next
+            result+="None"
+            return result
+        return "None"
+    
+    def __len__(self):
+        """gives you length of your linklist"""
+        return self.n
+
+    def __getitem__(self,index):
+        """
+        if pos<self.n:
+            counter=pos
+            travle=self.head
+            while 0<counter<=pos:
+                travle=travle.next
+                counter-=1
+            #print(travle.data)
+            return travle.data
+        raise IndexError("Index is out of bound")"""
+        pos=0
+        travle=self.head
+        while travle:
+            if pos == index:
+                return travle.data
+            travle=travle.next
+            pos+=1
+        raise IndexError("Index is out of range")
+    
+    def __delitem__(self,index):
+        if index < self.n:
+            if index == 0 :
+                self.head=self.head.next
+                self.n-=1
+                return True
+            counter=1
+            travle=self.head
+            while counter != index:
+                travle=travle.next
+                counter+=1
+            travle.next=travle.next.next
+            return True
+        raise IndexError("Index out of range")
+    def __update_head(self,nod):
+        self.head=nod
+        self.n+=1
+        return True
+    
+    def __travle_node(self,nod):
+        travle=self.head
+        while travle.next:
+            travle=travle.next
+        travle.next=nod
+        self.n+=1
+        return True
+    def __get_pos(self,value):
+        counter=0
+        if self.head:
+            travle=self.head
+            while travle:
+                if travle.data == value:
+                    return counter
+                counter+=1
+                travle=travle.next
+            return -1
+        return -1   
+    def index(self,value):
+        travle=self.head
+        pos=0
+        while travle:
+            if travle.data == value:
+                print(pos)
+                return pos
+            travle=travle.next
+            pos+=1
+        raise ValueError("Value is not in Linklist")
+
+    def insert_head(self,value):
+        """inserting data at head point"""
+        node=Node(value)
+        node.next=self.head
+        self.__update_head(node)
+        return True
+
+    def append(self,value):
+        """inserting data at tail of linklist
+        like appending data in list
+        also you can use below function
+        node=Node(value)
+        if self.head:
+            self.__travle_node(node)
+            return True
+        self.__update_head(node)
+        return True
+        """
+        node=Node(value)
+        if self.head:
+            travle=self.head
+            while travle.next:
+                travle=travle.next
+            travle.next=node
+            self.n+=1
+            return True
+        self.head=node
+        self.n+=1
+        return True
+
+    def insert(self,index,value):
+        """insert given value in mention index(left side ofprevious data)
+        if index is big it will append the data as well if LL is empty it will
+        append the data"""
+        node=Node(value)
+        pos=0
+        travle=self.head
+        second_var=None
+        while travle:
+            if index==0:
+                self.insert_head(value)
+                return True
+            if pos == index:
+                node.next=travle
+                second_var.next=node
+                self.n+=1
+                return True
+            second_var=travle
+            travle=travle.next
+            pos+=1
+        self.append(value)
+        return True
+
+    def insert_r(self,index,value):
+        """insert the data at right side of given index number
+        like if pass 2 index it will add it to 3 index in case if 
+        empty list or big index it will append the value"""
+        node=Node(value)
+        pos=0
+        travle=self.head
+        while travle:
+            if pos == index:
+                node.next=travle.next
+                travle.next=node
+                self.n+=1
+                return True
+            travle=travle.next
+            pos+=1
+        self.append(value)
+        return True
+
+    def add_before(self,value,new_value):
+        """Add the data before the given value it will search
+        the given value and if found add new_value on left/before side"""
+        if self.head:
+            if self.head.data == value:
+                self.insert_head(new_value)
+                return True
+            node=Node(new_value)
+            travle=self.head
+            while travle.next:
+                if travle.next.data == value:
+                    node.next=travle.next
+                    travle.next=node
+                    self.n+=1
+                    return True
+                travle=travle.next
+            raise ValueError("Value not in link-list")
+        raise ValueError("Value not in link-list")
+
+    def add_after(self,value,new_value):
+        """Add the data after the given value it will search
+        the given value and if found add new_value after the found value"""
+        if self.head:
+            node=Node(new_value)
+            travle=self.head
+            while travle:
+                if travle.data == value:
+                    node.next=travle.next
+                    travle.next=node
+                    self.n+=1
+                    return True
+                travle=travle.next
+            raise ValueError("Value not in link-list")
+        raise ValueError("Value not in link-list")
+
+    def clear(self):
+        """clear the link-list"""
+        self.head=None
+        self.n=0
+        return True
+            
+    def remove_head(self):
+        if self.head:
+            self.head=self.head.next
+            self.n-+1
+            return True
+        raise ValueError("LinkList is Empty")
+
+    def remove(self,value):
+        if self.head:
+            if self.head.data == value:
+                self.head=self.head.next
+                self.n-=1
+                return True
+            elif self.head.next:
+                travle=self.head
+                while travle.next:
+                    if travle.next.data == value:
+                        travle.next=travle.next.next
+                        self.n-=1
+                        return True
+                    travle=travle.next
+                raise ValueError("remove(x) X not in linklist")
+        raise ValueError("Link-list is empty")
+
+
+    def pop(self):
+        if self.head:
+            if self.head.next:
+                travle=self.head
+                while travle.next.next:
+                    travle=travle.next
+                print(travle.next.data)
+                travle.next=None
+                self.n-=1
+                return True
+            print(self.head.data)
+            self.head=None
+            self.n-=1
+            return True
+        raise ValueError('Link-list is empty')
+
+    def remove_2(self,value):
+        if self.head:
+            pos=self.__get_pos(value)
+            print("this is POS",pos)
+            if pos==0:
+                self.head=self.head.next
+                self.n-=1
+                return True
+            elif pos>0:
+                travle=self.head
+                second_travle=None
+                for i in range(pos):
+                    second_travle=travle
+                    travle=travle.next
+                second_travle.next=travle.next
+                self.n-=1
+                return True
+            raise ValueError("remove_2(X) X not found")
+        raise ValueError("Link-list is empty") 
+
+    def find(self,value):
+        self.index(value)
+        return True 
+
+
+
+        
+    
+a=my_ll()
+
+a.insert_head(20)
+a.insert_head(30)
+a.insert_head(10)
+a.append('append')
+# # print(a.n)
+a.insert_r(4,"OO")
+print(a)
+# a.add_left(30,100)
+# a.insert(0,20)
